5cf3446b5dd9201a5b566b23723475cbdbdba9a8
Jiajie Chen <jiegec@qq.com>
2017-11-15 16:41:29 +0800

    Add link to desktop and reformat code

--- AutoRemountNTFS/AppDelegate.m
+++ AutoRemountNTFS/AppDelegate.m
@@ -19,57 +19,56 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
     [[NSBundle mainBundle] addToLoginItems];
-    
+
     statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     [statusItem setTitle:@"NTFS"];
-    
+
     menu = [[NSMenu alloc] init];
     [statusItem setMenu:menu];
-    
+
     ntfsDevices = [[NSMutableArray alloc] init];
     [self checkNTFS];
 }
 
-- (void)checkNTFS
-{
+- (void)checkNTFS {
     NSPipe *pipe = [NSPipe pipe];
     NSFileHandle *file = pipe.fileHandleForReading;
-    
+
     NSTask *task = [[NSTask alloc] init];
     task.launchPath = @"/sbin/mount";
     task.arguments = @[];
     task.standardOutput = pipe;
-    
+
     [task launch];
-    
+
     NSData *data = [file readDataToEndOfFile];
     [file closeFile];
-    
-    NSString *output = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+
+    NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSArray *lines = [output componentsSeparatedByString:@"\n"];
     [menu removeAllItems];
     [ntfsDevices removeAllObjects];
-    
+
     NSTimeInterval time = 10;
-    
+
     for (NSString *line in lines) {
         if ([line rangeOfString:@"ntfs"].location != NSNotFound) {
             NSString *devPath = [line componentsSeparatedByString:@" "][0];
             NSString *mountPath = [line componentsSeparatedByString:@" "][2];
             [ntfsDevices addObject:mountPath];
-            
+
             NSMenuItem *item = [[NSMenuItem alloc] init];
             [item setTitle:[NSString stringWithFormat:@"%@", mountPath]];
-            
-            [item setTag:[ntfsDevices count]-1];
-            
+
+            [item setTag:[ntfsDevices count] - 1];
+
             NSMenuItem *open = [[NSMenuItem alloc] init];
             [open setTitle:NSLocalizedString(@"\tOpen", nil)];
             [open setAction:@selector(open:)];
             NSMenuItem *eject = [[NSMenuItem alloc] init];
             [eject setTitle:NSLocalizedString(@"\tEject", nil)];
             [eject setAction:@selector(eject:)];
-            
+
             [menu addItem:item];
             [menu addItem:open];
             [menu addItem:eject];
@@ -78,24 +77,24 @@
                 notification.title = NSLocalizedString(@"New NTFS Read-only Volume Detected", nil);
                 notification.informativeText = NSLocalizedString(@"Now making it writable.", nil);
                 [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification:notification];
-                
-                NSString * path= [[NSBundle mainBundle] pathForResource:@"remount" ofType:@"sh"];
-                
+
+                NSString *path = [[NSBundle mainBundle] pathForResource:@"remount" ofType:@"sh"];
+
                 STPrivilegedTask *privilegedTask = [[STPrivilegedTask alloc] init];
                 [privilegedTask setLaunchPath:@"/bin/sh"];
-                NSArray *args = [NSArray arrayWithObjects:path,
-                                 devPath,
-                                 mountPath,nil];
+                NSArray *args = @[path,
+                        devPath,
+                        mountPath];
                 [privilegedTask setArguments:args];
-                
+
                 OSStatus err = [privilegedTask launch];
                 if (err == errAuthorizationSuccess) {
                     [NSThread sleepForTimeInterval:0.5];
-                    NSUserNotification *notification = [[NSUserNotification alloc] init];
-                    notification.title = NSLocalizedString(@"NTFS Read-only Volume Now Writable", nil);
-                    notification.informativeText = NSLocalizedString(@"Now it is writable.", nil);
-                    [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification:notification];
-                    
+                    NSUserNotification *notification2 = [[NSUserNotification alloc] init];
+                    notification2.title = NSLocalizedString(@"NTFS Read-only Volume Now Writable", nil);
+                    notification2.informativeText = NSLocalizedString(@"Now it is writable.", nil);
+                    [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification:notification2];
+
                     [[NSWorkspace sharedWorkspace] openFile:[mountPath stringByAppendingString:@"/"]];
                 } else if (err == errAuthorizationCanceled) {
                     time = 60;
@@ -103,13 +102,13 @@
             }
         }
     }
-    
+
     [menu addItem:[NSMenuItem separatorItem]];
     NSMenuItem *item = [[NSMenuItem alloc] init];
     [item setTitle:@"Quit"];
     [item setAction:@selector(terminate:)];
     [menu addItem:item];
-    
+
     [NSTimer scheduledTimerWithTimeInterval:time
                                      target:self
                                    selector:@selector(checkNTFS)
@@ -118,28 +117,28 @@
 }
 
 - (void)eject:(id)sender {
-    NSMenuItem *item = (NSMenuItem*)sender;
+    NSMenuItem *item = (NSMenuItem *) sender;
     NSInteger tag = [item tag];
-    NSString *mountPath = [ntfsDevices objectAtIndex:tag];
-    
+    NSString *mountPath = ntfsDevices[tag];
+
     NSUserNotification *notification = [[NSUserNotification alloc] init];
     notification.title = [NSString stringWithFormat:NSLocalizedString(@"Now ejecting NTFS Volume %@", nil), mountPath];
     notification.informativeText = NSLocalizedString(@"Please wait.", nil);
     [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification:notification];
-    
+
     NSPipe *pipe = [NSPipe pipe];
     NSFileHandle *file = pipe.fileHandleForReading;
-    
+
     NSTask *task = [[NSTask alloc] init];
     task.launchPath = @"/usr/sbin/diskutil";
     task.arguments = @[@"eject", mountPath];
     task.standardOutput = pipe;
-    
+
     [task launch];
-    
+
     [file readDataToEndOfFile];
     [file closeFile];
-    
+
     notification = [[NSUserNotification alloc] init];
     notification.title = [NSString stringWithFormat:NSLocalizedString(@"NTFS Volume %@ Is Ejected", nil), mountPath];
     notification.informativeText = NSLocalizedString(@"You can unplug the device now.", nil);
@@ -147,10 +146,10 @@
 }
 
 - (void)open:(id)sender {
-    NSMenuItem *item = (NSMenuItem*)sender;
+    NSMenuItem *item = (NSMenuItem *) sender;
     NSInteger tag = [item tag];
-    NSString *mountPath = [ntfsDevices objectAtIndex:tag];
-    
+    NSString *mountPath = ntfsDevices[tag];
+
     [[NSWorkspace sharedWorkspace] openFile:[mountPath stringByAppendingString:@"/"]];
 }
 
--- AutoRemountNTFS/remount.sh
+++ AutoRemountNTFS/remount.sh
@@ -1,3 +1,4 @@
 /sbin/umount $2
 /bin/mkdir $2
 /sbin/mount -t ntfs -o rw,auto,nobrowse $1 $2
+/bin/ln -s -f $2 ~/Desktop
