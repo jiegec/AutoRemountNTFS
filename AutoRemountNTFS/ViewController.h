c14d713d9ab13b96c3294f324840c43eeada9514
Jiajie Chen <jiegec@qq.com>
2017-12-04 14:39:41 +0800

    Release 1.2 and add license

--- AutoRemountNTFS/ViewController.h
+++ AutoRemountNTFS/ViewController.h
@@ -1,10 +1,20 @@
-//
-//  ViewController.h
-//  AutoRemountNTFS
-//
-//  Created by MacBookAir on 20/08/2017.
-//  Copyright © 2017 Jiege Chen. All rights reserved.
-//
+/*
+ *  This file is part of AutoRemountNTFS
+ *  Copyright (c) 2017 AutoRemountNTFS's authors
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #import <Cocoa/Cocoa.h>
 
